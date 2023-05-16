960cd7dda59412df4fa5b16898d2d158f4410639
Anthony <acr1191@gmail.com>
2023-05-16 13:34:40 -0700

    end game if a previously clicked card is clicked again. set high score and reset game when game ends

--- src/App.js
+++ src/App.js
@@ -6,20 +6,17 @@ import { useState, useEffect } from 'react';
 function App() {
   const [score, setScore] = useState(0);
   const [highScore, setHighScore] = useState(0);
-
-  const [activeCards, setActiveCards] = useState([0, 1, 2]);
-
-  const newHighScore = (newHighScore) => {
-    setHighScore(newHighScore);
-  }
+  const [activeCards, setActiveCards] = useState([]);
+  const [clickedCards, setClickedCards] = useState([]);
+  const [isGameOver, setIsGameOver] = useState(false);
 
   useEffect(() => {
-    const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+    const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
     let newCards = [];
 
     const shuffleCards = () => {
       newCards.length = 0;
-      while(newCards.length < 3){
+      while(newCards.length < 3 + score){
         let index = Math.floor(Math.random() * cards.length);
         let newCard = cards.slice(index, index + 1)[0];
         if(!newCards.some(card => card === newCard)) {
@@ -33,28 +30,46 @@ function App() {
       setScore(score + 1);
     };
 
-    const handleCardClick = () => {
-      incrementScore();
-      shuffleCards();
+    const trackClickedCards = (e) => {
+      setClickedCards([...clickedCards, e.target.dataset.value]);
     }
 
-    document.querySelector('.shuffle').addEventListener('click', shuffleCards);
+    const handleCardClick = (e) => {
+      if(clickedCards.some(card => card === e.target.dataset.value)) {
+        setIsGameOver(true);
+      } else {
+        trackClickedCards(e);
+        shuffleCards();
+        incrementScore();
+      }
+    }
+
+    document.querySelector('.start').addEventListener('click', shuffleCards);
     document.querySelectorAll('.card').forEach(card => card.addEventListener('click', handleCardClick));
 
     return () => {
-      document.querySelector('.shuffle').removeEventListener("click", shuffleCards);
+      document.querySelector('.start').removeEventListener("click", shuffleCards);
       document.querySelectorAll('.card').forEach(card => card.removeEventListener('click', handleCardClick));
     };
 
-  }, [activeCards, score]);
+  }, [activeCards, score, clickedCards]);
+
+  useEffect(() => {
+    setActiveCards([]);
+    if(score > highScore)
+      setHighScore(score);
+    setScore(0);
+    setClickedCards([]);
+    setIsGameOver(false);
+  }, [isGameOver]);
 
   return (
     <div className="App">
       <Scoreboard score={score} highScore={highScore}/>
-      <button className='shuffle' >Shuffle cards</button>
+      <button className='start'>Start Game</button>
       <div className='play-area'>
         {activeCards.map(card => {
-          return <Card name={card} />
+          return <Card name={card} value={card}/>
         })}
       </div>
     </div>
--- src/components/Card.js
+++ src/components/Card.js
@@ -3,9 +3,10 @@ import React from 'react';
 
 const Card = (props) => {
     const name = props.name;
+    const value = props.value;
 
     return (
-        <div className='card'>
+        <div className='card' data-value={value}>
             <img src='' alt='img'/>
             <p>{name}</p>
         </div>
